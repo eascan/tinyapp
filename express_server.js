896f0dedbe5ddece3ba89ada6d272eb969f50c85
Edward <edward.ascan@me.com>
2021-01-20 20:35:11 +0000

    added nodemon

--- express_server.js
+++ express_server.js
@@ -34,8 +34,17 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  // console.log(shortURL)
+  // console.log(req.body.longURL)
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(`/urls/:${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
