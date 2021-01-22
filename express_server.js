899c3bcb6f1b54b43ab5fc228cf26ab4f9f71158
Edward <edward.ascan@me.com>
2021-01-22 03:10:37 +0000

    installed and required bcrypt

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
+const bcrypt = require('bcrypt');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
